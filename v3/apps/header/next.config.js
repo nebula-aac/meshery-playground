d4d609cafdd5f63061a362e74709e034f3b496fa
Antonette Caldwell <acald.nebula@gmail.com>
2023-06-10 09:25:32 -0500

    added header component

--- v3/apps/header/next.config.js
+++ v3/apps/header/next.config.js
@@ -1,4 +1,50 @@
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+const dependencies = require("./package.json").dependencies
+
+const remotes = (isServer) => {
+    const location = isServer ? "ssr" : "chunks"
+    return {
+        dashboard: `dashboard@http://localhost:3000/_next/static/${location}/remoteEntry.js`
+    }
+}
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack(config, options) {
+        const { webpack } = options
+        if (!options.isServer) {
+            config.plugins.push(
+                new NextFederationPlugin({
+                    name: 'header',
+                    remotes: remotes(options.isServer),
+                    exposes: {
+                        './nav': './src/components/Header'
+                    },
+                    filename: 'static/chunks/remoteEntry.js',
+                    shared: {
+                        react: {
+                            singleton: true,
+                            requiredVersion: dependencies.react
+                        },
+                        'react-dom': {
+                            singleton: true,
+                            requiredVersion: dependencies['react-dom'],
+                            eager: true,
+                        },
+                        'react/jsx-runtime': {
+                            singleton: true,
+                            requiredVersion: '0',
+                        }
+                    },
+                    extraOptions: {
+                        exposePages: true,
+                        automaticAsyncBoundary: true,
+                    },
+                })
+            )
+        }
+        return config
+    }
+}
 
 module.exports = nextConfig
---
+++ v3/apps/header/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+export default function Header() {
+  return (
+    <Fragment>
+      <div>
+        <h2>Header</h2>
+      </div>
+    </Fragment>
+  )
+}
